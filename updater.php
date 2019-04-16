95fc8a8563e6ce3fd0cd8fe92bca565619b63298
ntd-file-share <ntd-file-share@users.noreply.github.com>
2019-04-16 13:53:15 +0200

    Update updater.php

--- updater.php
+++ updater.php
@@ -33,6 +33,23 @@ class Smashing_Updater {
   public function authorize( $token ) {
     $this->authorize_token = $token;
   }
+  // get version tag from github
+  private function get_repository_info() {
+    if ( is_null( $this->github_response ) ) { // Do we have a response?
+      $request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
+      if( $this->authorize_token ) { // Is there an access token?
+        $request_uri = add_query_arg( 'access_token', $this->authorize_token, $request_uri ); // Append it
+      }
+      $response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
+      if( is_array( $response ) ) { // If it is an array
+        $response = current( $response ); // Get the first item
+      }
+      if( $this->authorize_token ) { // Is there an access token?
+        $response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] ); // Update our zip url with token
+      }
+      $this->github_response = $response; // Set it to our property
+    }
+  }
   // initialize function
   public function initialize() {
     add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'modify_transient' ), 10, 1 );
@@ -61,45 +78,44 @@ class Smashing_Updater {
     }
     return $transient; // Return filtered transient
   }
-}
-// info about plugin and new version
-public function plugin_popup( $result, $action, $args ) {
-  if( ! empty( $args->slug ) ) { // If there is a slug
-    if( $args->slug == current( explode( '/' , $this->basename ) ) ) { // And it's our slug
-      $this->get_repository_info(); // Get our repo info
-      // Set it to an array
-      $plugin = array(
-        'name'              => $this->plugin["Name"],
-        'slug'              => $this->basename,
-        'version'           => $this->github_response['tag_name'],
-        'author'            => $this->plugin["AuthorName"],
-        'author_profile'    => $this->plugin["AuthorURI"],
-        'last_updated'      => $this->github_response['published_at'],
-        'homepage'          => $this->plugin["PluginURI"],
-        'short_description' => $this->plugin["Description"],
-        'sections'          => array(
-          'Description'   => $this->plugin["Description"],
-          'Updates'       => $this->github_response['body'],
-        ),
-        'download_link'     => $this->github_response['zipball_url']
-      );
-      return (object) $plugin; // Return the data
+  // info about plugin and new version
+  public function plugin_popup( $result, $action, $args ) {
+    if( ! empty( $args->slug ) ) { // If there is a slug
+      if( $args->slug == current( explode( '/' , $this->basename ) ) ) { // And it's our slug
+        $this->get_repository_info(); // Get our repo info
+        // Set it to an array
+        $plugin = array(
+          'name'              => $this->plugin["Name"],
+          'slug'              => $this->basename,
+          'version'           => $this->github_response['tag_name'],
+          'author'            => $this->plugin["AuthorName"],
+          'author_profile'    => $this->plugin["AuthorURI"],
+          'last_updated'      => $this->github_response['published_at'],
+          'homepage'          => $this->plugin["PluginURI"],
+          'short_description' => $this->plugin["Description"],
+          'sections'          => array(
+            'Description'   => $this->plugin["Description"],
+            'Updates'       => $this->github_response['body'],
+          ),
+          'download_link'     => $this->github_response['zipball_url']
+        );
+        return (object) $plugin; // Return the data
+      }
     }
+    return $result; // Otherwise return default
   }
-  return $result; // Otherwise return default
-}
-// things to do after installation, like making sure, the plugin ist still active
-public function after_install( $response, $hook_extra, $result ) {
-  global $wp_filesystem; // Get global FS object
+  // things to do after installation, like making sure, the plugin ist still active
+  public function after_install( $response, $hook_extra, $result ) {
+    global $wp_filesystem; // Get global FS object
 
-  $install_directory = plugin_dir_path( $this->file ); // Our plugin directory
-  $wp_filesystem->move( $result['destination'], $install_directory ); // Move files to the plugin dir
-  $result['destination'] = $install_directory; // Set the destination for the rest of the stack
+    $install_directory = plugin_dir_path( $this->file ); // Our plugin directory
+    $wp_filesystem->move( $result['destination'], $install_directory ); // Move files to the plugin dir
+    $result['destination'] = $install_directory; // Set the destination for the rest of the stack
 
-  if ( $this->active ) { // If it was active
-    activate_plugin( $this->basename ); // Reactivate
+    if ( $this->active ) { // If it was active
+      activate_plugin( $this->basename ); // Reactivate
+    }
+    return $result;
   }
-  return $result;
 }
-
 ?>
