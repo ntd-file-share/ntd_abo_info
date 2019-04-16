da4be62ef55f1c60edce278f0fff8baf66d0c09b
ntd-file-share <ntd-file-share@users.noreply.github.com>
2019-04-16 13:44:26 +0200

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,20 +1,105 @@
 <?php
 class Smashing_Updater {
+  // properties to hold plugin information
   protected $file;
   protected $plugin;
   protected $basename;
   protected $active;
-
+  // properties for repo information
+  private $username;
+  private $repository;
+  private $authorize_token;
+  private $github_response;
+  // constructor - filepath is required
   public function __construct( $file ) {
     $this->file = $file;
+    // make sure WP functions are already available
     add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );
     return $this;
   }
-
+  // setter for plugin information
   public function set_plugin_properties() {
     $this->plugin   = get_plugin_data( $this->file );
     $this->basename = plugin_basename( $this->file );
     $this->active   = is_plugin_active( $this->basename );
   }
+  // setters for repository information
+  public function set_username( $username ) {
+    $this->username = $username;
+  }
+  public function set_repository( $repository ) {
+    $this->repository = $repository;
+  }
+  public function authorize( $token ) {
+    $this->authorize_token = $token;
+  }
+  // initialize function
+  public function initialize() {
+    add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'modify_transient' ), 10, 1 );
+    add_filter( 'plugins_api', array( $this, 'plugin_popup' ), 10, 3);
+    add_filter( 'upgrader_post_install', array( $this, 'after_install' ), 10, 3 );
+  }
+  // check for update
+  public function modify_transient( $transient ) {
+
+    if( property_exists( $transient, 'checked') ) { // Check if transient has a checked property
+      if( $checked = $transient->checked ) { // Did WordPress check for updates?
+        $this->get_repository_info(); // Get the repo info
+        $out_of_date = version_compare( $this->github_response['tag_name'], $checked[$this->basename], 'gt' ); // Check if we're out of date
+        if( $out_of_date ) {
+          $new_files = $this->github_response['zipball_url']; // Get the ZIP
+          $slug = current( explode('/', $this->basename ) ); // Create valid slug
+          $plugin = array( // setup our plugin info
+            'url' => $this->plugin["PluginURI"],
+            'slug' => $slug,
+            'package' => $new_files,
+            'new_version' => $this->github_response['tag_name']
+          );
+          $transient->response[ $this->basename ] = (object) $plugin; // Return it in response
+        }
+      }
+    }
+    return $transient; // Return filtered transient
+  }
 }
+// info about plugin and new version
+public function plugin_popup( $result, $action, $args ) {
+  if( ! empty( $args->slug ) ) { // If there is a slug
+    if( $args->slug == current( explode( '/' , $this->basename ) ) ) { // And it's our slug
+      $this->get_repository_info(); // Get our repo info
+      // Set it to an array
+      $plugin = array(
+        'name'              => $this->plugin["Name"],
+        'slug'              => $this->basename,
+        'version'           => $this->github_response['tag_name'],
+        'author'            => $this->plugin["AuthorName"],
+        'author_profile'    => $this->plugin["AuthorURI"],
+        'last_updated'      => $this->github_response['published_at'],
+        'homepage'          => $this->plugin["PluginURI"],
+        'short_description' => $this->plugin["Description"],
+        'sections'          => array(
+          'Description'   => $this->plugin["Description"],
+          'Updates'       => $this->github_response['body'],
+        ),
+        'download_link'     => $this->github_response['zipball_url']
+      );
+      return (object) $plugin; // Return the data
+    }
+  }
+  return $result; // Otherwise return default
+}
+// things to do after installation, like making sure, the plugin ist still active
+public function after_install( $response, $hook_extra, $result ) {
+  global $wp_filesystem; // Get global FS object
+
+  $install_directory = plugin_dir_path( $this->file ); // Our plugin directory
+  $wp_filesystem->move( $result['destination'], $install_directory ); // Move files to the plugin dir
+  $result['destination'] = $install_directory; // Set the destination for the rest of the stack
+
+  if ( $this->active ) { // If it was active
+    activate_plugin( $this->basename ); // Reactivate
+  }
+  return $result;
+}
+
 ?>
