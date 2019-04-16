b718eda9f033364d689d8d643ca0125b0281fd08
ntd-file-share <48246945+ntd-file-share@users.noreply.github.com>
2019-04-16 16:50:08 +0200

    Update ntd_service_abo.php

--- ntd_service_abo.php
+++ ntd_service_abo.php
@@ -7,6 +7,27 @@ Version: 2.0.0
 Author URI: https://www.new-time.ch/
 */
 
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+	include_once 'updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'github-updater',
+			'api_url' => 'https://api.github.com/repos/ntd-file-share/ntd_abo_info',
+			'raw_url' => 'https://raw.github.com/ntd-file-share/ntd_abo_info/master',
+			'github_url' => 'https://github.com/ntd-file-share/ntd_abo_info',
+			'zip_url' => 'https://github.com/ntd-file-share/ntd_abo_info/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '6f637f478f70222eb8b65efaebd6230139609c1b',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
 
 function contact_SOAP($action){
 	require(plugin_dir_path( __FILE__ ) . 'includes/client_access.php');
